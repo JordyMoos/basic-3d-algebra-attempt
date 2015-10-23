4ba68162d3b655eee216a4e3ae9cb8eb542ec8f5
Jordy Moos <jordymoos@gmail.com>
2015-10-23 20:29:45 +0200

    Magnitude

--- Basic 3D Algebra/main.cpp
+++ Basic 3D Algebra/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <cmath>
 
 class Vector3
 {
@@ -7,12 +8,24 @@ private:
 	float x, y, z;
 
 public:
-	Vector3(float x, float y, float z)
-		: x(x), y(y), z(z)
-	{}
+	Vector3(float x, float y, float z);
 
 	friend std::ostream& operator<<(std::ostream& os, const Vector3& vec);
+
+public:
+
+	float getLength();
 };
+
+Vector3::Vector3(float x, float y, float z)
+	: x(x), y(y), z(z)
+{}
+
+// Magnitude
+float Vector3::getLength()
+{
+	return std::sqrt(std::pow(x, 2) +  std::pow(y, 2) + std::pow(z, 2));
+}
 
 std::ostream& operator<<(std::ostream& os, const Vector3& vec)
 {
@@ -26,6 +39,7 @@ int main(int argc, char* argv[])
 	Vector3 vec = Vector3(1, 2, 3);
 
 	std::cout << vec << std::endl;
+	std::cout << "Magnitude: " << vec.getLength() << std::endl;
 
 	system("pause");
 }
