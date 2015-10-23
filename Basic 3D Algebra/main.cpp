1571088a089c10f09832e1dd3c44b7b9491fd240
Jordy Moos <jordymoos@gmail.com>
2015-10-23 20:01:25 +0200

    Adding vector3

--- Basic 3D Algebra/main.cpp
+++ Basic 3D Algebra/main.cpp
@@ -1,8 +1,22 @@
 
 #include <iostream>
 
+class Vector3
+{
+private:
+	float x, y, z;
+
+public:
+	Vector3(float x, float y, float z)
+		: x(x), y(y), z(z)
+	{}
+
+
+};
+
 int main(int argc, char* argv[])
 {
+
 	std::cout << "hello";
 }
 
